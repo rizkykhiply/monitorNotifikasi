cf17b38a49d7b333cf7aa3803da6038584302982
rendsyah <rndyfrdynsyh@gmail.com>
2023-07-19 00:09:45 +0700

    add parameter

--- src/pages/api/transaction/index.ts
+++ src/pages/api/transaction/index.ts
@@ -4,15 +4,15 @@ import { NextApiRequest, NextApiResponse } from 'next';
 // Import Libs
 import { handlerProtectApi } from '@lib/protect';
 import { models } from '@lib/databases/models';
-import { API_OK } from '@lib/constants';
+import { API_OK, SERVICE_KODEPOS } from '@lib/constants';
 
 // Define Handler Transaction Api
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     // Transaction In
-    const getTransactionIn = await models.transaction.findAllTransactionIn();
+    const getTransactionIn = await models.transaction.findAllTransactionIn(SERVICE_KODEPOS);
 
     // Transaction Out
-    const getTransactionOut = await models.transaction.findAllTransactionOut();
+    const getTransactionOut = await models.transaction.findAllTransactionOut(SERVICE_KODEPOS);
 
     return API_OK(res, getTransactionIn);
 };
