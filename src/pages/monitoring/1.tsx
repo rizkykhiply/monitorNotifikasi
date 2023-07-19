06ca2a67923582b38a04a349c34620b555fc687a
rendsyah <rndyfrdynsyh@gmail.com>
2023-07-19 15:24:03 +0700

    add page monitoring

---
+++ src/pages/monitoring/1.tsx
@@ -0,0 +1,88 @@
+// Import Modules
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+// Import Interfaces
+import { PropsMonitoring } from './interfaces';
+
+// Import Hooks
+import { useHooksTransaction } from '@hooks/transaction';
+
+// Import Component
+import Navbar from '@components/Navbar/Navbar';
+import Footer from '@components/Footer/Footer';
+
+// Import Assets
+import DefaultImage from '../../../public/default-image.png';
+
+// Import Styles
+import styles from './styles.module.css';
+
+// Define Monitoring Page
+const MonitoringPage = (props: PropsMonitoring) => {
+    const { data } = useHooksTransaction(props.kodePos, props.gate);
+
+    return (
+        <>
+            <Head>
+                <title>Monitoring - SCG Indonesia</title>
+            </Head>
+            <section className={styles.monitoring_section}>
+                <Navbar />
+                <div className={styles.monitoring_container}>
+                    <span className={styles.monitoring_title}>Monitoring System</span>
+                    <span className={styles.monitoring_sub_title}>(Gate IN)</span>
+                    <div className={styles.monitoring_system}>
+                        <div>
+                            <div className={styles.monitoring_data}>
+                                <span className={styles.monitoring_data_title}>Nama</span>
+                                <span className={styles.monitoring_data_text}>
+                                    {data?.nama_visitor ? data.nama_visitor : data?.nama_karyawan ? data.nama_karyawan : '-'}
+                                </span>
+                            </div>
+                            <div className={styles.monitoring_data}>
+                                <span className={styles.monitoring_data_title}>Divisi</span>
+                                <span className={styles.monitoring_data_text}>{data?.divisi ? data.divisi : '-'}</span>
+                            </div>
+                            <div className={styles.monitoring_data}>
+                                <span className={styles.monitoring_data_title}>No Polisi</span>
+                                <span className={styles.monitoring_data_text}>
+                                    {data?.no_polisi_visitor
+                                        ? data.no_polisi_visitor
+                                        : data?.no_polisi_karyawan
+                                        ? data.no_polisi_karyawan
+                                        : '-'}
+                                </span>
+                            </div>
+                            <div className={styles.monitoring_data}>
+                                <span className={styles.monitoring_data_title}>Time</span>
+                                <span className={styles.monitoring_data_text}>{data?.dateIn ? data.dateIn : '-'}</span>
+                            </div>
+                        </div>
+                        <div className={styles.monitoring_image}>
+                            <Image src={DefaultImage} alt="Image" width={500} height={350} />
+                        </div>
+                        {/* <div className={styles.monitoring_image}>
+                        <Image src={data?.imageIn} alt="Image" width={500} height={350} />
+                    </div> */}
+                    </div>
+                </div>
+                <Footer />
+            </section>
+        </>
+    );
+};
+
+// Define SSR Monitoring Page
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    return {
+        props: {
+            kodePos: 'PK2',
+            gate: 'in',
+        },
+    };
+};
+
+// Export Monitoring Page
+export default MonitoringPage;
---
+++ src/pages/monitoring/interfaces/index.ts
@@ -0,0 +1,5 @@
+// Define Props Monitoring
+export interface PropsMonitoring {
+    kodePos: string;
+    gate: string;
+}
